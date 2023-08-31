9a4f419bd67b7b7475d748a8482b0b218c1f1505
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-08-31 16:16:30 +0530

    status fix

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,8 +1,8 @@
 const JWT = require("jsonwebtoken");
-const User = require("../Models/User")
+const User = require("../Models/User");
 const {
-    HashPassword,
-    ComparePassword,
+  HashPassword,
+  ComparePassword,
 } = require("../Middlewares/authpassword");
 
 // cloudinary.config({
@@ -11,121 +11,122 @@ const {
 //     api_secret: process.env.api_secret,
 // });
 
-
 const CreateAccount = async (req, res) => {
-    try {
-        const { FName, LName, Email, PhoneNo, Password } = req.body;
+  try {
+    const { FName, LName, Email, PhoneNo, Password } = req.body;
 
-        if (!FName || !LName || !Email || !PhoneNo || !Password) {
-            return res.status(500).send({
-                message: "Kindly please fill all information...",
-                status: false,
-            });
-        }
-
-
-        if (
-            Email.match(
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-            )
-        )
-            return res
-                .status(500)
-                .send({ message: "Wrong email format", status: false });
-
-        if (
-            Password.match(
-                / ^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-            )
-        )
-            return res
-                .status(500)
-                .send({ message: "Wrong password format", status: false });
-
-        const CheckEmail = await User.findOne({ Email: Email.toLowerCase() });
-
-        if (CheckEmail) {
-            return res
-                .status(500)
-                .send({ message: "Email allready exist", status: false });
-        }
-
-        const hashPassword = await HashPassword(Password);
-        // console.log(hashPassword);
-        await new User({
-            FName,
-            LName,
-            Email: Email.toLowerCase(),
-            PhoneNo,
-            Password: hashPassword,
-            Role: "Student",
-        }).save();
-
-        return res
-            .status(200)
-            .send({ message: "Account Successfully Created", status: true });
-    } catch (error) {
-        console.log(error);
+    if (!FName || !LName || !Email || !PhoneNo || !Password) {
+      return res.status(500).send({
+        message: "Kindly please fill all information...",
+        status: false,
+      });
     }
+
+    if (
+      Email.match(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+      )
+    )
+      return res
+        .status(500)
+        .send({ message: "Wrong email format", status: false });
+
+    if (
+      Password.match(
+        / ^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+      )
+    )
+      return res
+        .status(500)
+        .send({ message: "Wrong password format", status: false });
+
+    const CheckEmail = await User.findOne({ Email: Email.toLowerCase() });
+
+    if (CheckEmail) {
+      return res
+        .status(500)
+        .send({ message: "Email allready exist", status: false });
+    }
+
+    const hashPassword = await HashPassword(Password);
+    // console.log(hashPassword);
+    await new User({
+      FName,
+      LName,
+      Email: Email.toLowerCase(),
+      PhoneNo,
+      Password: hashPassword,
+      Role: "Student",
+    }).save();
+
+    return res
+      .status(200)
+      .send({ message: "Account Successfully Created", status: true });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //Login Account
 const LoginUser = async (req, res) => {
-    try {
-        const { Email, Password } = req.body;
-        if (!Email || !Password) {
-            return res.status(400).send({
-                message: "Kindly please enter your email and password",
-                status: false,
-            });
-        }
-
-        const UserCheck = await User.findOne({ Email: Email.toLowerCase() });
-
-        if (!UserCheck)
-            return res
-                .status(400)
-                .send({ message: "Not vailed user ", status: false });
-
-        const match = await ComparePassword(Password, UserCheck.Password);
-
-        if (!match)
-            return res.status(500).send({ message: "Wrong Detail", status: false });
-
-        const token = await JWT.sign(
-            { _id: UserCheck._id },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: "7d",
-            }
-        );
-
-        return res.status(200).send({
-            message: "Login Successfully",
-            token,
-            status: true,
-        });
-    } catch (error) {
-        console.log(error);
+  try {
+    const { Email, Password } = req.body;
+    if (!Email || !Password) {
+      return res.status(400).send({
+        message: "Kindly please enter your email and password",
+        status: false,
+      });
     }
+
+    const UserCheck = await User.findOne({ Email: Email.toLowerCase() });
+
+    if (!UserCheck)
+      return res
+        .status(400)
+        .send({ message: "Not vailed user ", status: false });
+
+    const match = await ComparePassword(Password, UserCheck.Password);
+
+    if (!match)
+      return res.status(500).send({ message: "Wrong Detail", status: false });
+
+    const token = await JWT.sign(
+      { _id: UserCheck._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    return res.status(200).send({
+      message: "Login Successfully",
+      token,
+      status: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-
 const FormCheck = async (req, res) => {
-    try {
-        const statusCheck = await User.findById({ _id : req.user._id }, { FormId: 1 })
+  try {
+    const statusCheck = await User.find({ _id: req.user._id });
 
-        if (statusCheck) return res.status(200).send({ message: "allredy Submiited", status: false })
-        else return res.status(200).send({ message: "Form not created ", status: true })
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
+    if (statusCheck.formId)
+      return res
+        .status(200)
+        .send({ message: "allredy Submiited", status: false });
+    else
+      return res
+        .status(200)
+        .send({ message: "Form not created ", status: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    CreateAccount,
-    LoginUser,
-    FormCheck
-}
+  CreateAccount,
+  LoginUser,
+  FormCheck,
+};
